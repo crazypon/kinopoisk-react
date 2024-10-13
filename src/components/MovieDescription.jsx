e37b6dde3ea108d32aaad52a129b5c04599171f0
Ilnur Fanisovich <ilnuryamaletdinov349@gmail.com>
2024-10-13 14:01:59 +0500

    moving to linux

--- src/components/MovieDescription.jsx
+++ src/components/MovieDescription.jsx
@@ -8,6 +8,7 @@ const MovieDescription = () => {
     const [currentMovie, setCurrentMovie] = useState([]);
     const [loading, setLoading] = useState(true);
 
+
     useEffect( () => {
         fetch('http://localhost:8080/app/v1/movies/' + movieId)
         .then( response => {
@@ -19,6 +20,7 @@ const MovieDescription = () => {
         .then( movie => {
             setCurrentMovie(movie);
             setLoading(false);
+            
             console.log(currentMovie);
         })
         .catch( error => {
@@ -29,34 +31,45 @@ const MovieDescription = () => {
     
     return (
         <div className="row">
-            <h3 className="movieTitle">{currentMovie.title}</h3>
-            <div className="col col-lg-4 col-md-6 col-sm-12">
-                <img src={"/" + currentMovie.imagePath} className="img-fluid" alt="movie poster" />
-            </div>
-            <div className="col col-lg-8 col-md-6 col-sm-12">
-                <p>Budget: {currentMovie.budget}</p>
-                <p>Starring: {currentMovie.mainCharacter}</p>
-                <p>Film Director: {currentMovie.filmDirector}</p>
-                <p>Genre: {currentMovie.genre}</p>
-                <div className="ratings">
-                    <p>IMDB: {currentMovie.imdbrating} </p>
-                    <p>RT: {currentMovie.rottenTomatoes}</p>
-                    <p>KP: {currentMovie.localRating}</p>
+            {loading ? (
+                <h1>Loading...</h1>
+            ) : (
+            <>
+                <h3 className="movieTitle">{currentMovie.title}</h3>
+                <div className="col col-lg-4 col-md-6 col-sm-12">
+                    <img src={"/" + currentMovie.imagePath} className="img-fluid" alt="movie poster" />
                 </div>
-                <br />
-                <div className="movieButtons">
-                    <button className="btn btn-primary">watch later</button>
-                    <button className="btn btn-primary">rate</button>     
-                </div>
+                <div className="col col-lg-8 col-md-6 col-sm-12">
+                    <p>Budget: {currentMovie.budget}</p>
+                    <p>Starring: {currentMovie.mainCharacter}</p>
+                    <p>Film Director: {currentMovie.filmDirector}</p>
+                    <p>Genre: {currentMovie.genre}</p>
 
-                <br />
-                <br />
-                <h4>Description</h4>
-                <p>{currentMovie.movieDescription}</p>
-            </div>
-            <div className="col col-lg-12 col-sm-12 col-md-12">
-                <h4>Comments</h4>
-            </div>
+                    <div className="ratings">
+                        <p>IMDB: {currentMovie.imdbrating} </p>
+                        <p>RT: {currentMovie.rottenTomatoes}</p>
+                        <p>KP: {currentMovie.localRating}</p>
+                    </div>
+
+                    <br />
+
+                    <div className="movieButtons">
+                        <button className="btn btn-primary">watch later</button>
+                        <button className="btn btn-primary">rate</button>     
+                    </div>
+
+                    <br />
+                    <br />
+
+                    <h4>Description</h4>
+                    <p>{currentMovie.movieDescription}</p>
+                </div>
+                <div className="col col-lg-12 col-sm-12 col-md-12">
+                    <h4>Comments {console.log(currentMovie)}</h4>
+                </div>
+            </>
+            )}
+
         </div>
     )
 }
